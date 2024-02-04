41a571d2c1d599f500e7a44c419085d2b9b2c965
SlashedWasTaken <123648269+SlashedWasTaken@users.noreply.github.com>
2024-02-04 12:18:42 -0500

    Update script.js

--- LongVideole/script.js
+++ LongVideole/script.js
@@ -558,7 +558,7 @@ function addRow(choiceData, result) {
     // Track Length
     const lengthCell = document.createElement('td')
     lengthCell.classList.add('length-cell')
-    lengthCell.innerText = mintoHour + ":" + time(choiceData.length)
+    lengthCell.innerText = mintoHour + ":" + secondsToMin(choiceData.length)
     lengthCell.className += " " + result["length"]
 
 
