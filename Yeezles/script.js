18bd3e222c27d58a1edd7248364b0e992b17e76d
SlashedWasTaken <123648269+SlashedWasTaken@users.noreply.github.com>
2024-02-03 22:33:43 -0500

    Update script.js

--- Yeezles/script.js
+++ Yeezles/script.js
@@ -313,7 +313,7 @@ async function doubleTrouble() {
 // MUST BE SAME NUMBER AS OBJECTS IN JSON FILE. MATH SECTION. math section. Math Section.
 function newMysterySong() {
     Math.seedrandom(today);
-    mysteryNumber = Math.floor(Math.random() * 5) + 1;
+    mysteryNumber = Math.floor(Math.random() * 269) + 1;
 
     console.log(today)
     //console.log(mysteryNumber + " is created.");
