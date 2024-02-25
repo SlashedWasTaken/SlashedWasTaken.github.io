beb187e5647d49d5b4cb670c1c7495edde36ea92
SlashedWasTaken <123648269+SlashedWasTaken@users.noreply.github.com>
2024-02-25 17:03:59 -0500

    Update autocomplete.js

--- LongVideole/autocomplete.js
+++ LongVideole/autocomplete.js
@@ -281,8 +281,7 @@ const songTitles = [
     "Staying Alive 10 Hour Version",
     "Disco Lounge - Kevin MacLeod - 2 HOURS | Download Link",
     "Nikola Tesla and Albert Einstein Meme 10 Hours",
-    "Amazing!! 【ﾌﾞﾙｽｺﾌｧｰ薺,まゆむし,Jinmenusagi,FUNKY髭HANK】5 Hours",
-
+    
 ]
 
 
