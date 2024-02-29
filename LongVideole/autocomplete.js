7773043266ede5cf65f49af87cffd3163c30c36d
SlashedWasTaken <123648269+SlashedWasTaken@users.noreply.github.com>
2024-02-29 16:29:57 -0500

    Update autocomplete.js

--- LongVideole/autocomplete.js
+++ LongVideole/autocomplete.js
@@ -287,6 +287,7 @@ const songTitles = [
     "Dragon Ball Z: The Ultimate Review - The Cell Saga",
     "The Carebear - Everywhere in The Beginning of Nowhere Stages 1-7 (The Final Edition)",
     "Tally Hall - Banana Man (1 hour version) (something we all needed)",
+    "The Caretakers - Nowhere At The Millennium Of Space - Stages 1-6 (Complete edition)",
     
 ]
 
