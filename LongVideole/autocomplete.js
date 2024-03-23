c0a5e572595e73274081dbfb73d1647bc8d9ec01
SlashedWasTaken <123648269+SlashedWasTaken@users.noreply.github.com>
2024-03-22 22:36:19 -0400

    Update autocomplete.js

--- LongVideole/autocomplete.js
+++ LongVideole/autocomplete.js
@@ -302,6 +302,7 @@ const songTitles = [
     "YOTSUBA: The 5 year-old who taught me love again",
     "Ten Thousand Digits of Pi in Morse Code in 3 Hours and 14 minutes",
     "pilotredsun - 4 & 1/2 Hour Playlist/Mega Mix",
+    "Orpheusftw's Entire King of the Hill YouTube Poop Collaboration",
     
 ]
 
