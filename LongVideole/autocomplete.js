2a6619b2d601b374b3d14ff56d0824accddd26d4
SlashedWasTaken <123648269+SlashedWasTaken@users.noreply.github.com>
2024-11-18 08:47:13 -0500

    Update autocomplete.js

--- LongVideole/autocomplete.js
+++ LongVideole/autocomplete.js
@@ -357,7 +357,7 @@ const songTitles = [
     "SlashedPort's Magnum Opus",
     "THE PRICE IS NICE: a steam dream - Part 1 - Bad Steam Game Price Guessing GAMESHOW (WAYNERADIOTV)",
     "Creed - Higher [1 HOUR]",
-    "DELETING THIS VIDEO AT 1 MILLION VIEWS - 63,505 LEFT",
+    "DELETING THIS VIDEO AT 1 MILLION VIEWS - 10,535 LEFT",
     "Earth, Wind & Fire - September stretched to 10 hours",
     "Xbox 360 stretched to 1 hour",
     "Man in Cave (Reupload)",
