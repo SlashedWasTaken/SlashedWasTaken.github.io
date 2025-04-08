23abd9ec54e67bd412db8df69b0e386d6a1f84e1
SlashedWasTaken <123648269+SlashedWasTaken@users.noreply.github.com>
2025-04-08 00:20:09 -0400

    Update autocomplete.js

--- LongVideole/autocomplete.js
+++ LongVideole/autocomplete.js
@@ -426,6 +426,7 @@ const songTitles = [
     "sans. but it keeps getting faster",
     "Mario Kart Wii by Logan_US in 1:10:06 - Awesome Games Done Quick 2024",
     "The 28 Hour Journey to Beat Paper Mario's Hardest Challenge (Pre-Hooktail Pit of 100 Trials)",
+    "crystal edge but the wave is moderately longer",
     
 ]
 
