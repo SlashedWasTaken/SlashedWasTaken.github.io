51cc620848a9a2da5c10a7c94a6deaf9955b1485
SlashedWasTaken <123648269+SlashedWasTaken@users.noreply.github.com>
2025-06-02 01:54:58 -0400

    Update script.js

--- LongVideole/script.js
+++ LongVideole/script.js
@@ -1,7 +1,7 @@
 // MUST BE SAME NUMBER AS OBJECTS IN JSON FILE. MATH SECTION. math section. Math Section.
 function newMysterySong() {
     Math.seedrandom(today);
-    mysteryNumber = Math.floor(Math.random() * 455) + 1;
+    mysteryNumber = Math.floor(Math.random() * 456) + 1;
 
     console.log(today)
     //console.log(mysteryNumber + " is created.");
